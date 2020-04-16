dae174e5f8af4c5fa19770684ed478d62dc5a7c8
thabisoddx <thabisomoha@gmail.com>
2020-04-16 18:49:28 +0200

    Updates

---
+++ precache-manifest.78b80b42638c01334fccf6536e08ef5a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a0e1e0707639b220099df8bffc3e8e18",
+    "url": "/taevo-invoice-generator/index.html"
+  },
+  {
+    "revision": "7d3b9de9f23c65977998",
+    "url": "/taevo-invoice-generator/static/css/main.ddef580c.chunk.css"
+  },
+  {
+    "revision": "cc147461502395ba82ea",
+    "url": "/taevo-invoice-generator/static/js/2.78e736a6.chunk.js"
+  },
+  {
+    "revision": "6de07e5aec012398ec95763a9e7531f9",
+    "url": "/taevo-invoice-generator/static/js/2.78e736a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d3b9de9f23c65977998",
+    "url": "/taevo-invoice-generator/static/js/main.5169cade.chunk.js"
+  },
+  {
+    "revision": "0a0da9606a39a3b4fd96",
+    "url": "/taevo-invoice-generator/static/js/runtime-main.6d70f1bc.js"
+  },
+  {
+    "revision": "88d604b4a619f0407c22cfc3281903c0",
+    "url": "/taevo-invoice-generator/static/media/arrow-right-icon.88d604b4.svg"
+  },
+  {
+    "revision": "0c9746b717670f0f789df6ce12ac3944",
+    "url": "/taevo-invoice-generator/static/media/trash.0c9746b7.svg"
+  }
+]);
